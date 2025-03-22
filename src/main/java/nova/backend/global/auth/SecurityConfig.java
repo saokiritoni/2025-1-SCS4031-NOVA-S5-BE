202d0dcdb7f5efe9dc142a128a22801509354621
saokiritoni <soeun.love.lee@gmail.com>
2025-03-22 22:58:01 +0900

    [init] cors 및 security 설정

---
+++ src/main/java/nova/backend/global/auth/SecurityConfig.java
@@ -0,0 +1,58 @@
+package nova.backend.global.auth;
+
+import lombok.RequiredArgsConstructor;
+import nova.backend.global.auth.jwt.JwtAuthenticationEntryPoint;
+import nova.backend.global.auth.jwt.JwtAuthenticationFilter;
+import nova.backend.global.auth.jwt.JwtProvider;
+import nova.backend.global.config.CorsConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final CorsConfig corsConfig;
+    private final JwtProvider jwtProvider;
+
+    // 토큰 없이 접근 가능한 URL
+    private static final String[] whiteList = {"/",
+            "token/**",
+
+            /* swagger */
+            "swagger/**",
+            "swagger-ui/**",
+            "v3/api-docs/**",
+    };
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers(whiteList);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagementConfigurer ->
+                        sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(exceptionHandlingConfigurer ->
+                        exceptionHandlingConfigurer.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                        authorizationManagerRequestMatcherRegistry.anyRequest().authenticated())
+                .addFilter(corsConfig.corsFilter())
+                .addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new ExceptionHandlerFilter(), JwtAuthenticationFilter.class)
+                .build();
+    }
+}
