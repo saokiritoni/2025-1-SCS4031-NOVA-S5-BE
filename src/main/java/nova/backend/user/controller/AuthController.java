5aa30c9ae079830e3e680f5d7cd20ad2d51063e9
saokiritoni <soeun.love.lee@gmail.com>
2025-04-08 19:38:16 +0900

    [feat] #5 소셜 로그인

---
+++ src/main/java/nova/backend/user/controller/AuthController.java
@@ -0,0 +1,52 @@
+package nova.backend.user.controller;
+
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import nova.backend.global.common.SuccessResponse;
+import nova.backend.user.dto.request.UserLoginRequestDTO;
+import nova.backend.user.dto.request.UserTokenRequestDTO;
+import nova.backend.user.dto.response.UserTokenResponseDTO;
+import nova.backend.user.service.OauthService;
+import nova.backend.user.service.TokenService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+@RestController
+public class AuthController implements AuthApi {
+
+    private final OauthService oauthService;
+    private final TokenService tokenService;
+
+    // 임시 토큰 발급 API. 추후 로그인 기능이 완성되면 삭제할 예정
+    @PostMapping("/token/{userId}")
+    public ResponseEntity<SuccessResponse<?>> getTempToken(@PathVariable Long userId) {
+        UserTokenResponseDTO userToken = tokenService.issueTempToken(userId);
+        return SuccessResponse.ok(userToken);
+    }
+
+    // 소셜 로그인
+    @PostMapping("/login")
+    public ResponseEntity<SuccessResponse<?>> socialLogin(@RequestBody @Valid UserLoginRequestDTO userLoginRequest) {
+        UserTokenResponseDTO userToken = oauthService.socialLogin(userLoginRequest);
+        return SuccessResponse.ok(userToken);
+    }
+
+    // 로그아웃
+    @PostMapping("/logout")
+    public ResponseEntity<SuccessResponse<?>> logout(@AuthenticationPrincipal Long userId) {
+        tokenService.logout(userId);
+        return SuccessResponse.ok(null);
+    }
+
+    // 리프레쉬 토큰 재발급
+    @PostMapping("/reissue")
+    public ResponseEntity<SuccessResponse<?>> reissueToken(@RequestBody @Valid UserTokenRequestDTO uerTokenRequest) {
+        UserTokenResponseDTO userToken = tokenService.reissue(uerTokenRequest);
+        return SuccessResponse.ok(userToken);
+    }
+
+}
---
+++ src/main/java/nova/backend/user/service/TokenService.java
@@ -0,0 +1,103 @@
+package nova.backend.user.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.RequiredArgsConstructor;
+import nova.backend.global.auth.jwt.JwtProvider;
+import nova.backend.global.error.exception.*;
+import nova.backend.user.dto.request.UserTokenRequestDTO;
+import nova.backend.user.dto.response.UserTokenResponseDTO;
+import nova.backend.user.entity.Role;
+import nova.backend.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static nova.backend.global.error.ErrorCode.JSON_PARSING_FAILED;
+import static nova.backend.global.error.ErrorCode.USER_NOT_FOUND;
+
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class TokenService {
+    private final JwtProvider jwtProvider;
+    private final RedisTemplate<String, String> redisTemplate;
+    private final UserRepository userRepository;
+
+    @Value("${jwt.refresh-token-expire-time}")
+    private long REFRESH_TOKEN_EXPIRE_TIME;
+
+    public String issueNewAccessToken(Long userId) {
+        Role role = userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException(USER_NOT_FOUND))
+                .getRole();
+
+        return jwtProvider.getIssueToken(userId, role, true);
+    }
+
+    public String issueNewRefreshToken(Long userId) {
+        Role role = userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException(USER_NOT_FOUND))
+                .getRole();
+
+        return jwtProvider.getIssueToken(userId, role, false);
+    }
+
+    public UserTokenResponseDTO issueTempToken(Long userId) {
+        String accessToken = issueNewAccessToken(userId);
+        String refreshToken = issueNewRefreshToken(userId);
+        return UserTokenResponseDTO.of(accessToken, refreshToken);
+    }
+
+    public UserTokenResponseDTO issueToken(Long userId, Role role) {
+
+        String accessToken = jwtProvider.getIssueToken(userId, role, true);
+
+        // refresh token이 이미 존재하면 기존 토큰 사용하고, 없으면 새로 발급
+        String redisKey = "RT:" + userId;
+        String storedRefreshToken = redisTemplate.opsForValue().get(redisKey);
+
+        if (storedRefreshToken == null) {
+            storedRefreshToken = issueNewRefreshToken(userId);
+
+            redisTemplate.opsForValue().set(redisKey, storedRefreshToken, REFRESH_TOKEN_EXPIRE_TIME, TimeUnit.SECONDS);
+        }
+
+        return UserTokenResponseDTO.of(accessToken, storedRefreshToken);
+    }
+
+    public UserTokenResponseDTO reissue(UserTokenRequestDTO userTokenRequest){
+        Long userId;
+        try {
+            userId = Long.valueOf(jwtProvider.decodeJwtPayloadSubject(userTokenRequest.accessToken()));
+        } catch (JsonProcessingException e) {
+            throw new UnauthorizedException(JSON_PARSING_FAILED);
+        }
+
+        String refreshToken = userTokenRequest.refreshToken();
+        String redisKey = "RT:" + userId;
+
+        // 리프레시 토큰 검증 (리프레시 토큰 만료시 재로그인 필요)
+        jwtProvider.validateRefreshToken(refreshToken);
+
+        String storedRefreshToken = redisTemplate.opsForValue().get(redisKey);
+
+        // 요청된 리프레시 토큰과 저장된 redis 저장된 리프레시 토큰 비교 검증
+        jwtProvider.equalsRefreshToken(refreshToken, storedRefreshToken);
+
+        String newAccessToken = issueNewAccessToken(userId);
+        String newRefreshToken = issueNewRefreshToken(userId);
+
+        redisTemplate.opsForValue().set(redisKey, newRefreshToken, REFRESH_TOKEN_EXPIRE_TIME, TimeUnit.SECONDS);
+
+        return UserTokenResponseDTO.of(newAccessToken, newRefreshToken);
+    }
+
+    public void logout(Long userId) {
+        String redisKey = "RT:" + userId;
+        redisTemplate.delete(redisKey);
+    }
+}
