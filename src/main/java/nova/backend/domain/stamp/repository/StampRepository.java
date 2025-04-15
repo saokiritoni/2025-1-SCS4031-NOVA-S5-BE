f2b46aa1027a96a5c44321529fc849e31adaaacc
saokiritoni <soeun.love.lee@gmail.com>
2025-04-15 19:43:42 +0900

    [feat] #18 스탬프북-스탬프북ID 조회

--- src/main/java/nova/backend/domain/stamp/repository/StampRepository.java
+++ src/main/java/nova/backend/domain/stamp/repository/StampRepository.java
@@ -3,8 +3,11 @@ package nova.backend.domain.stamp.repository;
 import nova.backend.domain.stamp.entity.Stamp;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StampRepository extends JpaRepository<Stamp, Long> {
 
     int countByStampBook_StampBookId(Long stampBookId);
+    List<Stamp> findByStampBook_StampBookId(Long stampBookId);
 
 }
