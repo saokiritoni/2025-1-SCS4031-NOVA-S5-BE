8b594ce5a8195b4c26f4a297a9afd512156d4e2c
saokiritoni <soeun.love.lee@gmail.com>
2025-04-16 14:56:07 +0900

    [refactor] #25 필요없는 DTO 삭제

--- src/main/java/nova/backend/domain/user/dto/request/UserVerifyRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package nova.backend.domain.user.dto.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record UserVerifyRequestDTO(
-        @NotBlank
-        String studentNumber
-) {
-}
