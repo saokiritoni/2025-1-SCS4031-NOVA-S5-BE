69c30b417c5cb0641aad00e8947dd744a490e6a4
saokiritoni <soeun.love.lee@gmail.com>
2025-04-16 17:51:40 +0900

    [feat] #25 내 QR 조회

--- src/main/java/nova/backend/domain/user/controller/UserController.java
+++ src/main/java/nova/backend/domain/user/controller/UserController.java
@@ -3,7 +3,7 @@ package nova.backend.domain.user.controller;
 import lombok.RequiredArgsConstructor;
 import nova.backend.domain.user.dto.response.QrCodeResponseDTO;
 import nova.backend.domain.user.service.UserService;
-import nova.backend.global.auth.UserAuthentication;
+import nova.backend.global.auth.CustomUserDetails;
 import nova.backend.global.common.SuccessResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -18,10 +18,10 @@ public class UserController {
 
     @GetMapping("/my-qr")
     public ResponseEntity<SuccessResponse<?>> getMyQrCode(
-            @AuthenticationPrincipal(expression = "userId") Long userId) {
+            @AuthenticationPrincipal CustomUserDetails userDetails) {
+
+        Long userId = userDetails.getUserId();
         QrCodeResponseDTO qrCode = userService.getMyQrCode(userId);
         return SuccessResponse.ok(qrCode);
     }
-
-
 }
