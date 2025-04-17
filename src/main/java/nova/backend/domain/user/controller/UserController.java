6928368fd94184e892d8477a09f4065e91947e47
saokiritoni <soeun.love.lee@gmail.com>
2025-04-17 13:49:41 +0900

    [docs] #29 user controller 문서 작성

--- src/main/java/nova/backend/domain/user/controller/UserController.java
+++ src/main/java/nova/backend/domain/user/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
-public class UserController {
+public class UserController implements UserApi {
 
     private final UserService userService;
 
