a291504010d4f1fc67379b3f988d9454dfc71fbe
saokiritoni <soeun.love.lee@gmail.com>
2025-04-17 16:18:02 +0900

    [fix] #5 read-only 삭제

--- src/main/java/nova/backend/domain/user/service/UserService.java
+++ src/main/java/nova/backend/domain/user/service/UserService.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
