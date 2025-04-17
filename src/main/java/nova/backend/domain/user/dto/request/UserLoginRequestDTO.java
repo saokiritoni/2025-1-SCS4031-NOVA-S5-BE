05e6a2651bd736614a1161bed83bbc1d2f2cadc3
saokiritoni <soeun.love.lee@gmail.com>
2025-04-17 15:30:26 +0900

    [fix] #5 DTO 에 name, role 추가

--- src/main/java/nova/backend/domain/user/dto/request/UserLoginRequestDTO.java
+++ src/main/java/nova/backend/domain/user/dto/request/UserLoginRequestDTO.java
@@ -2,9 +2,12 @@ package nova.backend.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import nova.backend.domain.user.entity.Role;
 import nova.backend.domain.user.entity.SocialType;
 
 public record UserLoginRequestDTO(
         @NotBlank String code,
-        @NotNull SocialType socialType
+        @NotNull SocialType socialType,
+        @NotNull Role role,
+        String name
 ) {}
