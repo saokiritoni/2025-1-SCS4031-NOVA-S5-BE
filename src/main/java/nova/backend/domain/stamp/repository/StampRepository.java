19495c06706ad3f666a278b2dad9830a64132afb
saokiritoni <soeun.love.lee@gmail.com>
2025-05-09 03:09:17 +0900

    [feat] #58 최근 스탬프 3개 적립 기록

--- src/main/java/nova/backend/domain/stamp/repository/StampRepository.java
+++ src/main/java/nova/backend/domain/stamp/repository/StampRepository.java
@@ -1,6 +1,7 @@
 package nova.backend.domain.stamp.repository;
 
 import nova.backend.domain.stamp.entity.Stamp;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,6 +10,7 @@ public interface StampRepository extends JpaRepository<Stamp, Long> {
 
     int countByStampBook_StampBookId(Long stampBookId);
     List<Stamp> findByStampBook_StampBookId(Long stampBookId);
+    @EntityGraph(attributePaths = {"stampBook", "stampBook.cafe"})
     List<Stamp> findTop3ByStampBook_Cafe_CafeIdOrderByCreatedAtDesc(Long cafeId);
 
 }
