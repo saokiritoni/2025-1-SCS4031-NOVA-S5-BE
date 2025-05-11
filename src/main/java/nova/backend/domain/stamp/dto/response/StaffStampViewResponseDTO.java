fcbdac00c38969ec789f0e5a500da1b09a1c0f08
saokiritoni <soeun.love.lee@gmail.com>
2025-05-12 01:45:51 +0900

    [feat] #65 유저 조회 반환값 수정

--- src/main/java/nova/backend/domain/stamp/dto/response/StaffStampViewResponseDTO.java
+++ src/main/java/nova/backend/domain/stamp/dto/response/StaffStampViewResponseDTO.java
@@ -1,9 +1,28 @@
 package nova.backend.domain.stamp.dto.response;
 
+import nova.backend.domain.cafe.entity.Cafe;
+import nova.backend.domain.user.entity.User;
+
 import java.util.List;
 
 public record StaffStampViewResponseDTO(
+        String profilePictureUrl,
+        String name,
+        String characterType, // 카페의 캐릭터
+        int rewardCount,
         List<StampHistoryResponseDTO> history,
         List<RecentStampResponseDTO> recentStamps
 ) {
+    public static StaffStampViewResponseDTO from(User user, Cafe cafe, int rewardCount,
+                                                 List<StampHistoryResponseDTO> history,
+                                                 List<RecentStampResponseDTO> recentStamps) {
+        return new StaffStampViewResponseDTO(
+                user.getProfileImageUrl(),
+                user.getName(),
+                cafe.getCharacterType().name(),
+                rewardCount,
+                history,
+                recentStamps
+        );
+    }
 }
