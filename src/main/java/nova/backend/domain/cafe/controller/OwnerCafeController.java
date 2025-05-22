165a05cc296cf2bba68bb775df956386fd36c186
saokiritoni <soeun.love.lee@gmail.com>
2025-05-22 17:28:59 +0900

    [feat] #90 카페 커스텀 관련 get 추가

--- src/main/java/nova/backend/domain/cafe/controller/OwnerCafeController.java
+++ src/main/java/nova/backend/domain/cafe/controller/OwnerCafeController.java
@@ -2,15 +2,20 @@ package nova.backend.domain.cafe.controller;
 
 import lombok.RequiredArgsConstructor;
 import nova.backend.domain.cafe.dto.request.CafeRegistrationRequestDTO;
+import nova.backend.domain.cafe.dto.response.CafeDetailResponseDTO;
 import nova.backend.domain.cafe.entity.Cafe;
+import nova.backend.domain.cafe.repository.CafeRepository;
 import nova.backend.domain.cafe.service.OwnerCafeService;
 import nova.backend.domain.stampBook.dto.request.StampBookDesignUpdateRequestDTO;
 import nova.backend.global.auth.CustomUserDetails;
 import nova.backend.global.common.SuccessResponse;
+import nova.backend.global.error.exception.BusinessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import static nova.backend.global.error.ErrorCode.ENTITY_NOT_FOUND;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +23,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class OwnerCafeController implements OwnerCafeApi {
 
     private final OwnerCafeService ownerCafeService;
+    private final CafeRepository cafeRepository;
 
     @PostMapping("/register")
     public ResponseEntity<SuccessResponse<?>> registerCafe(
@@ -40,5 +46,22 @@ public class OwnerCafeController implements OwnerCafeApi {
         return SuccessResponse.ok("스탬프북 커스텀이 저장되었습니다.");
     }
 
+    @GetMapping("/stampbook-design")
+    public ResponseEntity<SuccessResponse<?>> getStampBookDesign(
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        Cafe cafe = cafeRepository.findById(userDetails.getSelectedCafeId())
+                .orElseThrow(() -> new BusinessException(ENTITY_NOT_FOUND));
+        return SuccessResponse.ok(cafe.getStampBookDesignJson());
+    }
+
+    @GetMapping("/detail")
+    public ResponseEntity<SuccessResponse<?>> getCafeDetail(
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        Cafe cafe = cafeRepository.findById(userDetails.getSelectedCafeId())
+                .orElseThrow(() -> new BusinessException(ENTITY_NOT_FOUND));
+        return SuccessResponse.ok(CafeDetailResponseDTO.fromEntity(cafe));
+    }
 
 }
