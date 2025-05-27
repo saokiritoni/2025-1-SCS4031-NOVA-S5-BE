ceb5c95e0c7b86f0fe4354a668d9e6b03bbe81ea
saokiritoni <soeun.love.lee@gmail.com>
2025-05-27 14:49:44 +0900

    [fix] #107 카페 목록 조회 영업 시간 함께 반환

--- src/main/java/nova/backend/domain/cafe/dto/response/CafeSummaryWithConceptDTO.java
+++ src/main/java/nova/backend/domain/cafe/dto/response/CafeSummaryWithConceptDTO.java
@@ -2,16 +2,22 @@ package nova.backend.domain.cafe.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import nova.backend.domain.cafe.dto.common.CafeBasicInfoDTO;
+import nova.backend.domain.cafe.dto.common.CafeOpenHourDTO;
+import nova.backend.domain.cafe.dto.common.CafeOperatingInfoDTO;
 import nova.backend.domain.cafe.entity.Cafe;
+
+import java.util.List;
 
 public record CafeSummaryWithConceptDTO(
         @JsonUnwrapped CafeBasicInfoDTO basicInfo,
-        String conceptIntroduction
+        String conceptIntroduction,
+        List<CafeOpenHourDTO> openHours
 ) {
     public static CafeSummaryWithConceptDTO from(Cafe cafe) {
         return new CafeSummaryWithConceptDTO(
                 CafeBasicInfoDTO.from(cafe),
-                cafe.getConceptIntroduction()
+                cafe.getConceptIntroduction(),
+                CafeOperatingInfoDTO.openHoursFrom(cafe)
         );
     }
 }
