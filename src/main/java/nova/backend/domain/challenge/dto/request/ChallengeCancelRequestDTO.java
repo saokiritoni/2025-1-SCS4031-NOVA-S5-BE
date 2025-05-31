43f625481aefb40afe7424ad6373a9686b02ebd9
saokiritoni <soeun.love.lee@gmail.com>
2025-06-01 01:04:55 +0900

    [refactor] #100 request 변수명 일치

--- src/main/java/nova/backend/domain/challenge/dto/request/ChallengeCancelRequestDTO.java
+++ src/main/java/nova/backend/domain/challenge/dto/request/ChallengeCancelRequestDTO.java
@@ -3,9 +3,9 @@ package nova.backend.domain.challenge.dto.request;
 import nova.backend.global.error.ErrorCode;
 import nova.backend.global.error.exception.BusinessException;
 
-public record ChallengeCancelRequestDTO(String qrCodeValue, int cancelCount) {
+public record ChallengeCancelRequestDTO(String qrCodeValue, int count) {
     public void validate() {
-        if (cancelCount <= 0) {
+        if (count <= 0) {
             throw new BusinessException(ErrorCode.INVALID_INPUT_VALUE);
         }
     }
